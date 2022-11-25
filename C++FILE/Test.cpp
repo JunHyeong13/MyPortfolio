adcc5e8b7737e02972efba4296c0b6a5c77d667e
JunHyeong13 <whwnsgud13@hanyang.ac.kr>
2022-11-25 14:03:42 +0900

    Setting for Arduino

--- C++FILE/Test.cpp
+++ C++FILE/Test.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello";
+    int N; // N개의 물건
+    int K;
+    int W, V;
+
+    // 물품의 수와 버틸 수 있는 무게 값.
+    cin >> N >> K;
+    for(int i = 0; i < 4; i++)
+    {
+        //if()
+    }
+    cout << V;
     return 0;
 }
