d047c4d0d53494e0db78d16e15c60935e339cffe
JunHyeong13 <whwnsgud13@hanyang.ac.kr>
2023-06-22 16:04:06 +0900

    PlayeMovemet
    Make a script for moving

--- Roll_A_Ball/Assets/Scripts/PlayerMovement.cs
+++ Roll_A_Ball/Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector3 m_Movement;
+    Animator m_Animator;
+    public float turnSpeed= 20f;
+    // Set-up the player wants to rotate
+    Quaternion m_Rotation = Quaternion.identity;
+    Rigidbody m_rb;
+   //public float level;
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();   
+        m_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        // Input the player movement setting
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         
+        // Also using 'Set' to set-up the player movement
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWaliking", isWalking);
+
+        // Set-up the player wants to go forward direction
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        // deltaPosition means '루트 모션으로 인한 프레임당 위치의 이동량'. 
+        m_rb.MovePosition(m_rb.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_rb.MoveRotation(m_Rotation);
     }
 }
